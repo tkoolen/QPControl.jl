5c0f218c926f4a6891fb3393111477ae0facae54
Twan Koolen <koolen.twan@gmail.com>
2018-01-31 15:50:19 -0500

    Get things to work with newer version of ForwardDiff, use RigidBodySim.

--- src/util.jl
+++ src/util.jl
@@ -1,5 +1,7 @@
 function val_deriv_deriv2(f, x)
-    ydual = f(ForwardDiff.Dual(ForwardDiff.Dual(x, one(x)), one(x)))
-    yddual = ForwardDiff.extract_derivative(ydual)
-    ForwardDiff.value(ForwardDiff.value(ydual)), ForwardDiff.value(yddual), ForwardDiff.extract_derivative(yddual)
+    inner = ForwardDiff.Dual(x, one(x))
+    outer = ForwardDiff.Dual(inner, one(x))
+    ydual = f(outer)
+    yddual = ForwardDiff.extract_derivative(Void, ydual)
+    ForwardDiff.value(ForwardDiff.value(ydual)), ForwardDiff.value(yddual), ForwardDiff.extract_derivative(Void, yddual)
 end
